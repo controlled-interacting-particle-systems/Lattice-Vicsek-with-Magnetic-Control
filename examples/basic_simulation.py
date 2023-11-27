3a21ffb9709b08f0ce428731195e1a010e488002
zakarya <zakaria.elkhiyati@gmail.com>
2023-11-27 17:59:43 +0100

    Fix basic_simulation script

--- examples/basic_simulation.py
+++ examples/basic_simulation.py
@@ -4,47 +4,67 @@ from vlmc.core.magnetic_field import MagneticField
 from vlmc.core.simulation import Simulation
 from tqdm import tqdm
 from examples.utils import *
+import numpy as np
 
 
 def main():
-    
-
     # Parameters for ParticleLattice
-    height, width = 100, 100
+    height, width = 10, 10
     obstacle_topology, sink_topology = "none", "central"
-    obstacle_walls, sink_walls = ["top", "left"], ["bottom", "right"]
-    obstacles, sinks = generate_lattice_topology(height, width, obstacle_topology, sink_topology, obstacle_walls, sink_walls)
+    obstacle_walls, sink_walls = ["top", "left","bottom", "right"], None
+    obstacles, sinks = generate_lattice_topology(
+        height, width, obstacle_topology, sink_topology, obstacle_walls, sink_walls
+    )
 
     lattice_params = {
         # include relevant parameters here
-        "width":width,
-        "height":height,
-        "density":0.1,
-        "obstacles":obstacles,
-        "sinks":sinks,
+        "width": width,
+        "height": height,
+        "density": 0.3,
+        "obstacles": None,
+        "sinks": None,
     }
 
-        # Creating a ParticleLattice instance
+    # Creating a ParticleLattice instance
     lattice = ParticleLattice(**lattice_params)
 
     # Creating a MagneticField instance
     magnetic_field = MagneticField()  # Add parameters if needed
 
     # Simulation parameters
-    g = 1.0  # Alignment sensitivity
-    v0 = 1.0  # Base transition rate
+    g = 2.0  # Alignment sensitivity
+    v0 = 100.0  # Base transition rate
     magnetic_field_interval = 10.0  # Time interval for magnetic field application
 
-    # Set up the simulation
-# Initialize the Simulation
-    simulation = Simulation(lattice_params, g, v0, magnetic_field, magnetic_field_interval)
+    # Initialize the Simulation
+    simulation = Simulation(g, v0, magnetic_field_interval, **lattice_params)
 
+    n_steps = int(10000) # Number of steps to run the simulation for
+    order_params = np.zeros(n_steps)
+    
+    print("Initial lattice")
+    print(lattice)
+    print(f"Initial order parameter: {simulation.lattice.compute_order_parameter()}")
+    # create a mapping between the event codes and the event names for printing purposes
+    # 0 is up 1 is left 2 is down 3 is right 4 is migrate
+    event_names = [
+        "turned up",
+        "turned left",
+        "turned down",
+        "turned right",
+        "migrated to the next cell",
+    ]
+    event_codes = [0, 1, 2, 3, 4]
+    event_map = dict(zip(event_codes, event_names))
 
-    # Run the simulation for a number of steps
-    num_steps = 10000
-    for step in tqdm(range(num_steps)):
-        simulation.run()
+    for _ in tqdm(range(n_steps)):  
+        event = simulation.run()
+        order_params[_] = simulation.lattice.compute_order_parameter()
+        # print(f"Event occurred: particle at {event[0], event[1]} {event_names[event[2]]}, at time {np.round(simulation.t}, 2)")
 
+    print("Final lattice")
+    print(lattice)
+    print(f"Final order parameter: {simulation.lattice.compute_order_parameter()}")
 
 if __name__ == "__main__":
     main()
