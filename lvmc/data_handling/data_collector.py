c760c40695c4f2c5d384607fc8df1582c90025d2
zakarya <zakaria.elkhiyati@gmail.com>
2023-12-06 17:17:52 +0100

    Revamp DataCollector class

--- lvmc/data_handling/data_collector.py
+++ lvmc/data_handling/data_collector.py
@@ -15,14 +15,18 @@ class DataCollector:
     Class for collecting and analyzing data from the simulation.
 
     Attributes:
-        data (pd.DataFrame): A DataFrame to store collected data.
+        event_data (pd.DataFrame): A DataFrame to store event data. Each row represents an event, and the columns are "TimeStep", "dt", "EventType", "X", "Y", and magnetic field direction.
+        metadata (dict): A dictionary to store metadata.
+        initial_state (np.ndarray): The initial state of the lattice.
     """
 
     def __init__(self):
         # Initialize an empty DataFrame to store data
-        self.data = pd.DataFrame(
-            columns=["TimeStep", "ElapsedTime", "OrientationCounts", "LatticeState"]
+        self.event_data = pd.DataFrame(
+            columns=["TimeStep", "dt", "EventType", "X", "Y", "MagneticField"]
         )
+        self.initial_state = None
+        self.metadata = {}
 
     def collect_data(self, simulation):
         """
@@ -32,80 +36,5 @@ class DataCollector:
         :type simulation: Simulation
         """
         # Collect data from simulation
-        time_step = simulation.time_steps
+        self.initial_state = copy.deepcopy(np.array(simulation.lattice.lattice))
         elapsed_time = simulation.t
-        orientation_counts = self._count_orientations(simulation.lattice)
-
-        # Deep copy the lattice state so that future changes to the lattice don't affect stored data
-        lattice_state = copy.deepcopy(simulation.lattice.grid)
-
-        # validate that data is a dataframe
-        if not isinstance(self.data, pd.DataFrame):
-            raise TypeError("data must be a pandas DataFrame")
-        # Append to data DataFrame
-        new_data = pd.DataFrame(
-            {
-                "TimeStep": [time_step],
-                "ElapsedTime": [elapsed_time],
-                "OrientationCounts": [orientation_counts],
-                "LatticeState": lattice_state,
-            }
-        )
-
-        self.data = pd.concat([self.data, new_data], ignore_index=True)
-
-    def _count_orientations(self, lattice):
-        """
-        Count the number of particles with each orientation on the lattice.
-
-        :param lattice: The lattice object.
-        :type lattice: Lattice
-        :return: Dictionary of orientation counts.
-        :rtype: dict
-        """
-        counts = {0: 0, 1: 0, 2: 0, 3: 0}
-        for y in range(lattice.height):
-            for x in range(lattice.width):
-                particle = lattice.grid[y, x]
-                if particle is not None:
-                    orientation = particle.get_orientation()
-                    counts[orientation] += 1
-        return counts
-
-    def _orientation_vector_from_counts(self, counts):
-        """
-        Compute the average orientation vector from the orientation counts.
-
-        :param counts: Dictionary of orientation counts.
-        :type counts: dict
-        :return: Average orientation vector.
-        :rtype: np.ndarray
-        """
-        # Compute the average orientation vector
-        orientation_vectors = [
-            ORIENTATION_TO_VECTOR[orientation] * count
-            for orientation, count in counts.items()
-        ]
-        average_orientation_vector = sum(orientation_vectors) / sum(counts.values())
-        return average_orientation_vector
-
-    def analyze_data(self):
-        """
-        Perform post-processing analysis on the collected data.
-        """
-        # compute the order parameter: module of the average orientation vector
-
-        # compute the average orientation vector for each time step
-        orientation_vectors = self.data["OrientationCounts"].apply(
-            self._orientation_vector_from_counts
-        )
-        order_parameters = orientation_vectors.apply(np.linalg.norm)
-        self.data["OrderParameter"] = order_parameters
-
-        # compute the cumulative mean order parameter for each time step
-        self.data["CumulativeMeanOrderParameter"] = (
-            self.data["OrderParameter"].expanding().mean()
-        )
-
-        # plot the cumulative mean order parameter as a function of time
-        self.data.plot(x="ElapsedTime", y="CumulativeMeanOrderParameter", legend=None)
