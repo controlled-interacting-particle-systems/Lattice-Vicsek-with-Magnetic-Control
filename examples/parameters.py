a65a65f4862f65ca7a4661ff156fa397f3b35ca0
zakarya <zakaria.elkhiyati@gmail.com>
2023-12-27 20:25:00 +0100

    Add parameters for ParticleLattice simulation

---
+++ examples/parameters.py
@@ -0,0 +1,11 @@
+# Parameters for ParticleLattice
+lattice_params = {
+    # include relevant parameters here
+    "width": 25,
+    "height": 50,
+    "density": 0.3,
+}
+
+# Simulation parameters
+g = 2.0  # Alignment sensitivity
+v0 = 100.0  # Base transition rate
