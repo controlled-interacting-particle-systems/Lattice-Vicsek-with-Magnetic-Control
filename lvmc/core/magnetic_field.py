13d93512bf6c2cf985127b66f446f264b0463308
zakarya <zakaria.elkhiyati@gmail.com>
2024-01-08 23:50:56 +0100

    Add type hints and update method signatures in MagneticField class

--- lvmc/core/magnetic_field.py
+++ lvmc/core/magnetic_field.py
@@ -1,9 +1,11 @@
+from lvmc.core.particle_lattice import ParticleLattice # for type hinting
+
 class MagneticField:
     """
     Class for managing magnetic field effects on particles.
     """
 
-    def __init__(self, initial_direction=0):
+    def __init__(self, initial_direction: int = 0):
         """
         Initialize MagneticField with an initial direction.
 
@@ -15,7 +17,7 @@ class MagneticField:
         """
         self.current_direction = initial_direction
 
-    def set_direction(self, direction):
+    def set_direction(self, direction: int) -> None:
         """
         Set the current direction of the magnetic field.
 
@@ -24,12 +26,12 @@ class MagneticField:
         """
         self.current_direction = direction
 
-    def apply(self, lattice):
+    def apply(self, lattice: 'ParticleLattice') -> None:
         """
         Apply the magnetic field to all particles on the lattice (a 90 degrees rotation in the prescribed direction).
 
         :param lattice: The lattice object.
-        :type lattice: Lattice
+        :type lattice: ParticleLattice
         """
         # the lattice is a 3D binary tensor with dimensions corresponding to layers/orientations, width, and height. orientation layers are up, down, left, and right in that order.
 
@@ -37,4 +39,12 @@ class MagneticField:
 
         # rotating the lattice is equivalent to shuffling the orientation layers
 
-        lattice.lattice[...] = lattice.lattice.roll(self.current_direction, dims=0)
+        lattice.particles[...] = lattice.particles.roll(self.current_direction, dims=0)
+
+    def get_current_direction(self) -> int:
+        """
+        Get the current direction of the magnetic field.
+
+        :return: int - The current direction of the magnetic field.
+        """
+        return self.current_direction
