acf93494874e9806bb4f56cd0e97653416bd8550
zakarya <zakaria.elkhiyati@gmail.com>
2024-01-12 12:41:42 +0100

    Update real-time visualization script with a an update_freq parameter

--- examples/real_time_viz_script.py
+++ examples/real_time_viz_script.py
@@ -1,32 +1,34 @@
 from lvmc.core.simulation import Simulation
 from lvmc.data_handling.data_collector import DataCollector
 from lvmc.data_handling.visualization import Visualization
-from parameters import g, v0, lattice_params, obstacles, sinks
+from parameters import *
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 
 
 def run_simulation():
     simulation = Simulation(g, v0, **lattice_params)
+    simulation.lattice.set_obstacles(obstacles)
     data_collector = DataCollector(simulation)
     visualizer = Visualization(data_collector)
 
-    plt.ion()  # Turn on interactive mode for real-time updating
 
-    n_steps = int(1e4)  # Number of steps to run the simulation for
+
+    n_steps = int(1e7)  # Number of steps to run the simulation for
+    update_freq = 1000  # Number of steps between each visualization update
 
     for step in tqdm(range(n_steps)):
         event = simulation.run()
-        data_collector.collect_event(event)
-        data_collector.collect_snapshot()
+        if step % update_freq == 0:
+            data_collector.collect_event(event)
+            data_collector.collect_snapshot()
 
-        # Fetch the latest snapshot for visualization
-        latest_snapshot = data_collector.data["snapshots"][-1][1]
-        visualizer.update_plot(latest_snapshot)
-        plt.draw()
-        plt.pause(0.00001)  # Pause to update the figure
+            # Fetch the latest snapshot for visualization
+            latest_snapshot = data_collector.data["snapshots"][-1][1]
+            visualizer.update_plot(latest_snapshot)
+            plt.draw()
+            plt.pause(0.000000001)  # Pause to update the figure
 
-    plt.ioff()  # Turn off interactive mode
 
 
 if __name__ == "__main__":
