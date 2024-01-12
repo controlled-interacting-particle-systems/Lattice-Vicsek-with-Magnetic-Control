40fde729d144129bff0a31e3f1a76098ac039987
zakarya <zakaria.elkhiyati@gmail.com>
2024-01-12 12:42:16 +0100

    Update lattice parameters and add topology setup

--- examples/parameters.py
+++ examples/parameters.py
@@ -1,11 +1,26 @@
 # Parameters for ParticleLattice
 lattice_params = {
     # include relevant parameters here
-    "width": 50,
-    "height": 25,
+    "width": 100,
+    "height": 10,
     "density": 0.3,
 }
 
 # Simulation parameters
 g = 2.0  # Alignment sensitivity
 v0 = 100.0  # Base transition rate
+
+
+# Topology setup
+from utils import *
+
+sink_walls = "right", "left"
+obstacle_walls = "top", "bottom"
+obstacles, sinks = generate_lattice_topology(
+    lattice_params["height"],
+    lattice_params["width"],
+    sink_walls=sink_walls,
+    obstacle_walls=obstacle_walls,
+)
+
+print(obstacles.shape)
