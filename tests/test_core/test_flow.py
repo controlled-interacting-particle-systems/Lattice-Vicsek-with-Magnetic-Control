d358aa5b202c69d6c9a98d6c20f1f5796955090e
zakarya <zakaria.elkhiyati@gmail.com>
2024-03-21 09:49:38 +0100

    Remove unnecessary print statements and imports in test_flow.py

--- tests/test_core/test_flow.py
+++ tests/test_core/test_flow.py
@@ -1,6 +1,6 @@
 from lvmc.core.particle_lattice import Orientation, ParticleLattice
 from lvmc.core.flow import Flow, PoiseuilleFlow
-from icecream import ic
+
 
 # Parameters for ParticleLattice
 lattice_params = {
@@ -16,10 +16,6 @@ def test_compute_tm():
     lattice = ParticleLattice(10, 5)
     lattice.add_particle(5, 2, Orientation.UP)
     tm = flow.compute_tm(lattice.occupancy_map)
-    print(tm[Orientation.RIGHT.value])
-    ic(lattice)
-    ic(lattice.occupancy_map)
-    ic(flow.velocity_field)
     assert tm.shape == (4, 5, 10)
     assert tm[Orientation.UP.value].any() == 0
     assert tm[Orientation.DOWN.value].any() == 0
